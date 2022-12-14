f1e44c6f517dfa27d973a36599d28134f2f3f0fc
Leonardo Figueiró <leonardofigueiro@MacBook-Pro-de-Leonardo.local>
2022-12-14 12:37:20 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { server } from "./server/server";
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+const port = process.env.PORT;
+
+server.listen(port, () => console.log(`Servidor escutando em http://localhost:${port}`));
