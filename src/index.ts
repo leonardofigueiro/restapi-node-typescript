3e1679d5a236eeb6fb047e52a0924fa63a13fac1
Leonardo Figueiró <leonardofigueiro@MacBook-Pro-de-Leonardo.local>
2022-12-21 09:57:47 -0400

    Providers and controllers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import {server} from './server/Server';
 import 'dotenv/config';
+import { Knex } from './server/database/knex';
 
 const port = process.env.PORT || 3333;
 
-server.listen(port, () => console.log(`Servidor escutando em http://localhost:${port}`));
+const startServer = () => {
+
+  server.listen(port, () => console.log(`Servidor escutando em http://localhost:${port}`));
+
+};
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  
+  Knex.migrate.latest().then(() => {
+    startServer();
+  })
+    .catch(console.log);
+} else {
+  startServer();
+}
+
+
